19aebbbe0e982e8d51912c0e952d1e0180ed4d41
Ahmed Afzal <ahmed.afzal2070@gmail.com>
2025-02-15 04:50:30 +0500

    add env

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,6 +1,7 @@
 var admin = require("firebase-admin");
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+console.log(serviceAccount);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
