45e925fcff34c17986712995ad8423561a681871
Ahmed Afzal <ahmed.afzal2070@gmail.com>
2025-02-15 04:04:46 +0500

    updae config

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,8 +1,6 @@
 var admin = require("firebase-admin");
-const fs = require("fs");
-const serviceAccount = JSON.parse(
-  fs.readFileSync(process.env.FIREBASE_SERVICE_ACCOUNT_KEY, "utf-8")
-);
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
