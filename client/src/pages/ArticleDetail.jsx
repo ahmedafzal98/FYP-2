48013934bebebceea07130e36dba894adcf2a82f
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2025-04-06 01:15:54 +0500

    update article detail page

--- client/src/pages/ArticleDetail.jsx
+++ client/src/pages/ArticleDetail.jsx
@@ -6,8 +6,17 @@ import {
   MessageCircle,
 } from "lucide-react";
 import Seperator from "../components/Sepeartor";
+import ActionButton from "../components/ActionButton";
 
 const ArticleDetail = () => {
+  const tags = [
+    "React Js Tutorials",
+    "React",
+    "React Js Development",
+    "Reactjs",
+    " Reactive Programming",
+    "React",
+  ];
   return (
     <section className="">
       <div className="flex flex-col items-center ">
@@ -80,6 +89,48 @@ const ArticleDetail = () => {
               beyond more UI.
             </span>
           </div>
+          <div className="w-full mt-[3%] flex gap-3 flex-wrap">
+            {tags &&
+              tags.map((tag, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="h-9 rounded-2xl flex justify-center items-center border border-[#F2F2F2] bg-[#F2F2F2] p-3"
+                  >
+                    <span className="text-[#242424] text-base">{tag}</span>
+                  </div>
+                );
+              })}
+          </div>
+          <div className="flex items-center w-full mt-4 justify-between">
+            <div className="flex gap-4">
+              <div className="w-12 h-12 bg-red-500 cursor-pointer hover:opacity-50 rounded-full mt-2"></div>
+              <div className="flex flex-col items-center">
+                <div className="flex w-full gap-4">
+                  <span className="text-[#242424] font-normal text-2xl cursor-pointer hover:underline">
+                    Written by Ahmed Afzal
+                  </span>
+                </div>
+                <div className="flex justify-between  w-full items-center gap-3">
+                  <span className="text-[#6B6B6B] cursor-pointer hover:underline">
+                    147 Followers
+                  </span>
+                  <span className="text-[#6B6B6B] cursor-pointer hover:underline">
+                    6 Following
+                  </span>
+                </div>
+              </div>
+            </div>
+            <ActionButton title="Follow" height="38" width="74" />
+          </div>
+          <div className="mt-5 w-5/6 mb-[5%]">
+            <span>
+              I'm a senior software engineer specializing in Java Spring Boot
+              Microservices. I'm also a dedicated blogger at javadzone.com where
+              I share java/springboot, etc.
+            </span>
+          </div>
+          <Seperator />
         </div>
       </div>
     </section>
